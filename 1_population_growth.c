0816e2073f50b3e30f17c78b4773bc9a4c6590e6
AR <aarokan@gmail.com>
2023-10-22 00:00:32 +0300

    prompt user for starting & ending population size

--- 1_population_growth.c
+++ 1_population_growth.c
@@ -4,8 +4,10 @@
 int main(void)
 {
     // Prompt the user for a starting population size
-    
+    int startingSize = get_int("Enter starting population size: ");
+
     // Prompt the user for a Ending population size
+    int endingSize = get_int("Enter ending population size: ");
 
     // Calculate number of years for the population to reach the size
 
