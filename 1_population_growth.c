d23850f7b005fa9da58f074b6394afb0843366cb
AR <aarokan@gmail.com>
2023-10-21 23:57:41 +0300

    inital commit

---
+++ 1_population_growth.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt the user for a starting population size
+    
+    // Prompt the user for a Ending population size
+
+    // Calculate number of years for the population to reach the size
+
+}
