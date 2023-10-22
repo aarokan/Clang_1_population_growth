b58d23b1db7092d5d2aca48456b485433cd08c48
AR <aarokan@gmail.com>
2023-10-22 23:56:01 +0300

    create calculate years function

--- 1_population_growth.c
+++ 1_population_growth.c
@@ -9,6 +9,19 @@ int main(void)
     // Prompt the user for a Ending population size
     int endingSize = get_int("Enter ending population size: ");
 
-    // Calculate number of years for the population to reach the size
+    // Call calculate years function
+}
 
+// Calculate number of years for the population to reach the size
+int CalculateYears(startSize, endSize)
+{
+    int years = 0;
+    while (startSize < endSize)
+    {
+        /* Say we have a population of n llamas. 
+        Each year, n / 3 new llamas are born, and n / 4 llamas pass away.*/
+        startSize += (startSize / 3) - (startSize / 4);
+        years += 1;
+    }
+    return years;       
 }
