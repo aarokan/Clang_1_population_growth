7d96ffd5ac0397001f2490d361e40ec97454a1c5
AR <aarokan@gmail.com>
2023-10-25 23:02:59 +0300

    diffrent solution

--- 1_population_growth.c
+++ 1_population_growth.c
@@ -3,10 +3,29 @@
 
 int main(void)
 {
-    // Prompt the user for a starting population size
-    
-    // Prompt the user for a Ending population size
+    int startSize, endSize;
+    // TODO: Prompt for start size
+    do
+    {
+        startSize = get_int("Start Size: ");
+    }
+    while (startSize < 9);
 
-    // Calculate number of years for the population to reach the size
+    // TODO: Prompt for end size
+    do
+    {
+        endSize = get_int("End Size: ");
+    }
+    while (endSize < startSize);
 
+    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    while (startSize < endSize)
+    {
+        startSize += (startSize / 3) - (startSize / 4);
+        years++;
+    }
+
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
 }
