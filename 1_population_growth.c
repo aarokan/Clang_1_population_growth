d58b3a9538d0a34ddc4b164ca8466244765b067e
AR <aarokan@gmail.com>
2023-10-25 20:11:12 +0300

    calculate years

--- 1_population_growth.c
+++ 1_population_growth.c
@@ -26,10 +26,7 @@ int calculateYears(startSize, endSize)
     {
         /* Say we have a population of n llamas. 
         Each year, n / 3 new llamas are born, and n / 4 llamas pass away.*/
-        startSize += (startSize / 3);
-        printf("startSize = %i\n", startSize);
-        startSize -= (startSize / 4);
-        printf("startSize = %i\n", startSize);
+        startSize += (startSize / 3) - (startSize /4);
         years += 1;
     }
     return years;       
